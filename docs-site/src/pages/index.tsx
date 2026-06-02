1d31633d1502f60045e47eda6dea252f24819300
Johny James <johnyjames9496@gmail.com>
2026-06-02 09:25:27 +0530

    fix get started link

--- docs-site/src/pages/index.tsx
+++ docs-site/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/generated/intro"
+            to="/docs/intro"
             style={{marginRight: '10px'}}>
             Get Started 🚀
           </Link>
